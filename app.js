80b22be01446e536e465b19369816cf5a4d58782
Mary8992 <mc046569@gmail.com>
2023-02-20 23:43:00 +0300

    responsive functionality added

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-const marker = document.querySelector('#marker');
-const item = document.querySelectorAll('ul li a');
+const marker = document.querySelector("#marker");
+const item = document.querySelectorAll("ul li a");
 
 function indicator(e) {
-    marker.style.top = e.offsetTop + 'px';
-    marker.style.width = e.offsetWidth + 'px';
+    marker.style.top = e.offsetTop + "px";
+    marker.style.width = e.offsetWidth + "px";
 }
 
-item.forEach(link => {
-    link.addEventListener('mousemove', (e) => {
-        indicator(e.target);
-    })
-})
+function indicator2(e) {
+    marker.style.left = e.offsetLeft + "px";
+    marker.style.width = e.offsetWidth + "px";
+}
+
+item.forEach((link) => {
+    link.addEventListener("mousemove", (e) => {
+        if (window.innerWidth >= 1000) {
+            indicator(e.target);
+        } else {
+            indicator2(e.target);
+        }
+    });
+});
